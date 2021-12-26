abecd930843fd852c61e5f150d1fda4e557f0b40
Evgeny Levenko <51111834+levenkoevgeny@users.noreply.github.com>
2021-12-26 19:06:54 +0300

    Update serializers.py

--- word_trainer/serializers.py
+++ word_trainer/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import Dictionary, Word
+from django.contrib.auth.models import User
 
 
 class DictionarySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Dictionary
-        fields = '__all__'
+        fields = ['id', 'owner', 'dictionary_name', 'get_words_count', 'date_created']
 
 
 class WordSerializer(serializers.ModelSerializer):
@@ -14,3 +15,10 @@ class WordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Word
         fields = ['dictionary', 'word_rus', 'word_eng']
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
