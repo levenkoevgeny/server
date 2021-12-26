83aeaab02be45b9f7d726abb7790ffd0838a919f
Evgeny Levenko <51111834+levenkoevgeny@users.noreply.github.com>
2021-12-26 19:07:17 +0300

    Update views.py

--- word_trainer/views.py
+++ word_trainer/views.py
@@ -1,13 +1,52 @@
 from rest_framework import viewsets
 from .models import Dictionary, Word
-from .serializers import DictionarySerializer, WordSerializer
+
+from django.contrib.auth.models import User
+from .serializers import DictionarySerializer, WordSerializer, UserSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework import permissions
 
 
 class DictionaryViewSet(viewsets.ModelViewSet):
     queryset = Dictionary.objects.all()
     serializer_class = DictionarySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'owner_id': ['exact'],
+    }
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class WordViewSet(viewsets.ModelViewSet):
     queryset = Word.objects.all()
     serializer_class = WordSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        'dictionary_id': ['exact'],
+    }
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+        })
+
