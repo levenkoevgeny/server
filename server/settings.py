e60508df0370422d5d64a009f9c1d035f6075e4c
Evgeny Levenko <51111834+levenkoevgeny@users.noreply.github.com>
2021-12-26 19:25:53 +0300

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-$t84v7+f_j+*@)sqrvs8*740mtw_^j-%(m=dse$t0#!gb-dwvj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
